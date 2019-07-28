682074247361dfc21c97bcf7fe562bfd645d8ba1
vootlavenky63 <vootla.venky63@gmail.com>
2019-07-28 13:35:32 +0530

    state true/false

---
+++ website/src/Insta.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Insta() {
+    return (
+        <div>
+            <div className="insta">
+            <div className=""></div>
+
+            </div>
+        </div>
+        
+    )
+}
