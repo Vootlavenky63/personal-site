ee03d1f25c89ade8b89ac3d972c8a0d65b0170a1
vootlavenky63 <vootla.venky63@gmail.com>
2019-07-28 09:01:30 +0530

    state

--- website/src/App.js
+++ website/src/App.js
@@ -7,20 +7,71 @@ import './App.css';
 import Top from './Top';
 import Bottom from './Bottom';
 import Likes from './Likes';
+import Prop from './Prop';
 
 
 
 function App() {
-  const[showmenu,setshowmenu]=useState(true)
+  const[showjson,setshowjson]=useState([
+    {
+      "id": 1,
+      "nAME": "Venkatesh",
+      "fOLLWERS": 33,
+      "fOLLOINGS": 33,
+      "lIKES": 100
+    },
+    {
+      "id": 2,
+      "nAME": "Avinash",
+      "fOLLWERS": 33,
+      "fOLLOINGS": 56,
+      "lIKES": 200
+    },
+    {
+      "id": 3,
+      "nAME": "Kiran",
+      "fOLLWERS": 45,
+      "fOLLOINGS": 65,
+      "lIKES": 2000
+    },
+    {
+      "id": 4,
+      "nAME": "Laxmi",
+      "fOLLWERS": 56,
+      "fOLLOINGS": 5,
+      "lIKES": 40000
+    },
+    {
+      "id": 5,
+      "nAME": "React",
+      "fOLLWERS": 65,
+      "fOLLOINGS": 87,
+      "lIKES": 49
+    }]
+  )
   // fetch('https://api.sheety.co/c07e747e-888f-46f8-a428-290f66ed5c97')
   // .then(response => response.json())
-  // .then(json => console.log(json,"<---- this is my json"))
-  // console.log("hiiiiiiiii")
-  console.log(showmenu)
-  console.log("<-------- show")
+  // .then(json =>setshowmenu(json))
+  // // console.log("hiiiiiiiii")
+  // console.log(showmenu)
+  // console.log("<-------- show")
+  console.log(showjson,"<------ json ")
+  showjson.map
 
   return (
     <div className="App">
+      {
+        showjson.map(item=>(
+         <div className="cardstyle"> <h1>{item.nAME}</h1><hr></hr>
+          <h1>{item.fOLLOINGS}</h1><hr></hr>
+          <h1>{item.lIKES}</h1>
+          </div>
+          
+        ))
+      }
+      {/* <Prop name={showjson}/> */}
+      
+      {/* <button onClick={()=>setshowjson("avinash")}>click</button> */}
     {/* <Header/> */}
     {/* <Hero name="venkatesh" position="fullstack developer"/> */}
     {/* <Hero name="avinash" position="mean developer"/> */}
---
+++ website/src/Prop.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Prop(props) {
+    return (
+        <div>
+            <h1>{props.name}</h1>
+        </div>
+    )
+}
