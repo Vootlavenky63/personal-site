2386a1580493988134ac0325e559a5657505dd24
vootlavenky63 <vootla.venky63@gmail.com>
2019-07-28 13:50:44 +0530

    menubar state

--- website/src/App.js
+++ website/src/App.js
@@ -1,11 +1,16 @@
 import React,{useState} from 'react'
+import Menu from './Menu';
 
 export default function App() {
   const[showmenu,setshowmenu]=useState(false)
 console.log(showmenu,"<-----show")
+
   return (
     <div>
-<button onClick={()=>setshowmenu(true)}>tagglemenu</button>      
+<button onClick={()=>setshowmenu(!showmenu)}>tagglemenu</button>   
+{showmenu ?(<Menu/>):null}
+
+
      
     </div>
   )
---
+++ website/src/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Menu() {
+    return (
+        <div>
+            <h1>hii am comming for menu component</h1>
+            
+        </div>
+    )
+}
